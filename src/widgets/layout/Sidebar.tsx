ff9f0021262f8cb203725bf3afe8c6db3104af4c
Турский Александр Сергеевич <namemilvvvvv4362@gmail.com>
2022-10-08 04:47:24 +0400

    fix

--- src/widgets/layout/Sidebar.tsx
+++ src/widgets/layout/Sidebar.tsx
@@ -1,122 +1,124 @@
 import React, { useState } from 'react';
 import s from './Sidebar.module.scss'
 import cn from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+const menuItems = [
+  {
+    title: 'Навигатор',
+    link: '/',
+    active: true,
+    img: '/icons/document-info.svg',
+    notifications: null,
+  },
+  {
+    title: 'Обучение',
+    active: false,
+    img: '/icons/target.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Проверка знаний',
+    active: false,
+    img: '/icons/users-alt.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Маркет',
+    active: false,
+    img: '/icons/shopping-bag.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Вебинары',
+    active: false,
+    img: '/icons/notes.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Обучение',
+    active: false,
+    img: '/icons/headphones.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Финансы',
+    active: false,
+    img: '/icons/suitcase.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Документы',
+    active: false,
+    img: '/icons/lightning.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Идеи',
+    active: false,
+    img: '/icons/star.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Объявления',
+    active: false,
+    img: '/icons/ads.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Еще',
+    active: false,
+    img: '/icons/subject.svg',
+    notifications: null,
+    link: '/t'
+  },
+  {
+    title: 'Автоматизация',
+    active: false,
+    img: '/icons/suitcase.svg',
+    notifications: null,
+    link: '/t'
+  },
+]
+
 
 const Sidebar = () => {
   const [active, setActive] = useState(false)
-  const menuItems = [
-    {
-      title: 'Навигатор',
-      active: true,
-      img: '/icons/document-info.svg',
-      notifications: null,
-    },
-    {
-      title: 'Обучение',
-      active: false,
-      img: '/icons/target.svg',
-      notifications: null,
-    },
-    {
-      title: 'Проверка знаний',
-      active: false,
-      img: '/icons/users-alt.svg',
-      notifications: null,
-    },
-    {
-      title: 'Маркет',
-      active: false,
-      img: '/icons/shopping-bag.svg',
-      notifications: null,
-    },
-    {
-      title: 'Вебинары',
-      active: false,
-      img: '/icons/notes.svg',
-      notifications: null,
-    },
-    {
-      title: 'Обучение',
-      active: false,
-      img: '/icons/headphones.svg',
-      notifications: null,
-    },
-    {
-      title: 'Финансы',
-      active: false,
-      img: '/icons/suitcase.svg',
-      notifications: null,
-    },
-    {
-      title: 'Документы',
-      active: false,
-      img: '/icons/lightning.svg',
-      notifications: null,
-    },
-    {
-      title: 'Идеи',
-      active: false,
-      img: '/icons/star.svg',
-      notifications: null,
-    },
-    {
-      title: 'Объявления',
-      active: false,
-      img: '/icons/ads.svg',
-      notifications: null,
-    },
-    {
-      title: 'Еще',
-      active: false,
-      img: '/icons/subject.svg',
-      notifications: null,
-    },
-    {
-      title: 'Автоматизация',
-      active: false,
-      img: '/icons/suitcase.svg',
-      notifications: null,
-    },
-  ]
-  const user = [
-    {
-      name: 'Станислав. И',
-      role: 'UI/UX Designer',
-      img: '/icons/acc.svg'
-    }
-  ]
+
   return (
-    <div className={active ? s.Sidebar : s.Sidebar_hidden} onClick={() => setActive(!active)}>
+    <div className={cn(s.Sidebar, { [s.Sidebar_hidden]: active })} onClick={() => setActive(!active)}>
       <div className={s.Sidebar_top}>
         <img alt="logo" src="/icons/logo.svg" className={s.logo}/>
         <ul className={s.Sidebar_menu}>
-          {menuItems.map((e, index) => {
-
-            return (
-              <li key={index} className={cn({ [s.active]: e.active })}>
-                <img src={`${e.img}`} alt="icon"/>
-                <p>{e.title}</p>
-              </li>
-            )
-          })}
+          {menuItems.map((e, index) => (
+            <NavLink end to={e.link} key={index} className={({ isActive }) => cn(s.link, { [s.active]: isActive })}>
+              <img src={`${e.img}`} alt="icon"/>
+              <p>{e.title}</p>
+            </NavLink>
+          ))}
         </ul>
       </div>
       <div className={s.Sidebar_bottom}>
-        <li className={s.notification}>
+        <a className={cn(s.link, s.notification)}>
           <img src="/icons/bell.svg" alt="icon"/>
           <p>Уведомления</p>
-        </li>
-        {user.map(e => {
-          return (
-            <div className={s.user}>
-              <img src={e.img}/>
-              <div className={s.text}>
-                <h3>{e.name}</h3>
-                <p>{e.role}</p>
-              </div>
-            </div>
-          )
-        })}
+        </a>
+        <NavLink to="/profile" className={({ isActive }) => cn(s.user, { [s.active]: isActive, [s.user__active]: isActive })}>
+          <img className="rounded-full" src="/icons/acc.svg"/>
+          <div className={s.text}>
+            <h3>Станислав. И</h3>
+            <p>UI/UX Designer</p>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
